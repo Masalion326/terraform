95c78f931bf72fcd7c3ed21225de4cc9dc677180
Masalion <masakuni0326@gmail.com>
2021-10-26 04:33:35 +0000

    feat : add security group

--- src/network.tf
+++ src/network.tf
@@ -1,15 +1,15 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-dev"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = var.vpc_azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
   
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
@@ -22,4 +22,18 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+
+
+module "security-group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = var.security_group_name
+  description = "Security group"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress_cidr_blocks      = var.ingress_cidr_blocks
+  ingress_rules            = var.ingress_rules
+  egress_rules             = var.egress_rules 
 }
