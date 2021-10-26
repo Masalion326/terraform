54c606173be422b6901bd01de97bd5db0c03b5e9
Masalion <masakuni0326@gmail.com>
2021-10-26 02:37:31 +0000

    test : add outputs and test for network

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,3 +1,12 @@
 output "name" {
   value = module.vpc.name
 }
+
+
+output "public_subnets_cidr_blocks" {
+  value = module.vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
--- test/networl_test.go
+++ test/networl_test.go
@@ -17,7 +17,21 @@ func TestNetwork(t *testing.T) {
 
 	// 	terraform.InitAndApply(t, terraformOptions)
 
-	actualNetworkName := terraform.Output(t, terraformOptions, "name")
-	assert.Equal(t, "vpc-dev", actualNetworkName)
+	actualVpcName := terraform.Output(t, terraformOptions, "name")
+	assert.Equal(t, "vpc-dev", actualVpcName)
+
+	expectedPrivateSubnetCIDRBlocks := []string{"10.0.1.0/24", "10.0.2.0/24"}
+	actualPrivateSubnetCIDRBlocks := terraform.OutputList(t, terraformOptions, "private_subnets_cidr_blocks")
+	assert.Equal(t, expectedPrivateSubnetCIDRBlocks, actualPrivateSubnetCIDRBlocks)
+
+	expectedPublicSubnetCIDRBlocks := []string{"10.0.101.0/24", "10.0.102.0/24"}
+	actualPublicSubnetCIDRBlocks := terraform.OutputList(t, terraformOptions, "public_subnets_cidr_blocks")
+	assert.Equal(t, expectedPublicSubnetCIDRBlocks, actualPublicSubnetCIDRBlocks)
+
+	// 	actualSubnetRegion := terraform.Output(t, terraformOptions, "subnetwork_region")
+	// 	assert.Equal(t, "asia-northeast1", actualSubnetRegion)
+
+	// 	actualSubnetCidr := terraform.Output(t, terraformOptions, "subnetwork_cidr")
+	// 	assert.Equal(t, "192.168.10.0/24", actualSubnetCidr)
 
 }
